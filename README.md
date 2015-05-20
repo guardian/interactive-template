4d4045795d498597b611cc9b4a9646b567075405
andymason <a.w.mason@gmail.com>
2015-05-20 12:02:39 +0100

    clean-up

---
+++ README.md
@@ -0,0 +1,27 @@
+# grunt-init-interactive-template
+
+> Create an interactive template project with [grunt-init] interactive-template
+
+## Installation
+If you haven't already done so, install [grunt-init](http://gruntjs.com/project-scaffolding).
+
+Once grunt-init is installed, place this template in your `~/.grunt-init/`
+directory. It's recommended that you use git to clone this template into that
+directory, as follows:
+
+```bash
+> git clone git@github.com:guardian/interactive-template.git ~/.grunt-init/interactive-template
+```
+
+## Usage
+
+At the command-line, cd into an empty directory, run this command and follow the prompts.
+
+```bash
+> grunt-init interactive-template 
+```
+
+_Note that this template will generate files in the current directory, so be 
+sure to change to a new directory first if you don't want to overwrite existing 
+files._
+
---
+++ template.js
@@ -0,0 +1,50 @@
+/*
+ * grunt-init-guardian-interactive
+ *
+ * Copyright (c) 2015 Guardian News & Media Ltd
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+// Basic template description.
+exports.description = 'Guardian interactive template.';
+
+// Template-specific notes to be displayed before question prompts.
+exports.notes = 'Project name should start with interactive- to distinguish ' +
+'it from other projects on the Guardian github repo.';
+
+// Template-specific notes to be displayed after question prompts.
+exports.after = 'You should now install project dependencies with _npm ' +
+  'install_ and then _bower install_. After that, you may start the project '+
+  'with _grunt_.';
+
+// Any existing file or directory matching this wildcard will cause a warning.
+exports.warnOn = '*';
+
+// The actual init template.
+exports.template = function(grunt, init, done) {
+
+  init.process({}, [
+    // Prompt for these values.
+    init.prompt('name', 'interactive-news-story'),
+    init.prompt('description', 'Guardian interactive project.'),
+    init.prompt('repository'),
+    init.prompt('author_name'),
+    init.prompt('licenses', 'Apache-2.0'),
+    init.prompt('s3_path', 'testing/uk/example/')
+  ], function(err, props) {
+
+    props.keywords = ['guardian', 'interactive', 'visuals'];
+
+    // Files to copy (and process).
+    var files = init.filesToCopy(props);
+
+    // Actually copy (and process) files.
+    init.copyAndProcess(files, props, {noProcess: 'libs/**'});
+
+    // All done!
+    done();
+  });
+
+};
